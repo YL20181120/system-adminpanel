25a18a7ad2cf71fca9cafd051951642e0a6b8e86
尤敬强 <youjingqiang@vip.qq.com>
2023-05-15 09:21:12 +0800

    prune logs

---
+++ src/Providers/FortifyServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace System\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+use System\Actions\Fortify\CreateNewUser;
+use System\Actions\Fortify\ResetUserPassword;
+use System\Actions\Fortify\UpdateUserPassword;
+use System\Actions\Fortify\UpdateUserProfileInformation;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string)$request->email;
+
+            return Limit::perMinute(5)->by($email . $request->ip());
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+    }
+}
